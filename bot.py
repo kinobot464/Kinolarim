7a32bf32168de23dc95497c98fde698956c25448
kinobot464 <suyunbekjobborov@gmail.com>
2025-07-06 17:33:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,36 @@
-from flask import Flask
-from threading import Thread
+from flask import Flask, request
 import telebot
+import os
 
 TOKEN = "8122568214:AAExyNrxhPOV1UUbMIdOL3EDKECaLq6X0lI"
 bot = telebot.TeleBot(TOKEN)
 
-app = Flask('')
+WEBHOOK_URL = 'https://kinobot1-1-jm8r.onrender.com/'  # Renderdagi URL manzilingiz
 
-@app.route('/')
-def home():
-    return "Bot ishlayapti."
+app = Flask(__name__)
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
+# Webhook sozlash
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        json_str = request.get_data().decode('UTF-8')
+        update = telebot.types.Update.de_json(json_str)
+        bot.process_new_updates([update])
+        return '!', 200
+    return 'Bot ishlayapti!', 200
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
 
-# Bu yerga bot handlerlaringizni yozasiz
+# /start komandasi uchun handler
 @bot.message_handler(commands=['start'])
-def start(message):
+def start_handler(message):
     bot.send_message(message.chat.id, "Salom! Kino kodini yuboring.")
 
-# Polling orqali ishga tushirish
-if __name__ == "__main__":
-    keep_alive()
-    bot.polling()
+# Render ishga tushganda webhookni sozlash
+@app.before_first_request
+def setup_webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+
+# Flask serverni ishga tushirish
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
