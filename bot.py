c7c6b25641a8468bf70f668a0cdfc1a8fd3a8730
kinobot464 <suyunbekjobborov@gmail.com>
2025-07-06 17:21:57 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,29 @@
-http://t.me/Kinolarrtv_bot
-8122568214:AAExyNrxhPOV1UUbMIdOL3EDKECaLq6X0lI
+from flask import Flask
+from threading import Thread
+import telebot
+
+TOKEN = "8122568214:AAExyNrxhPOV1UUbMIdOL3EDKECaLq6X0lI"
+bot = telebot.TeleBot(TOKEN)
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti."
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# Bu yerga bot handlerlaringizni yozasiz
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Salom! Kino kodini yuboring.")
+
+# Polling orqali ishga tushirish
+if __name__ == "__main__":
+    keep_alive()
+    bot.polling()
